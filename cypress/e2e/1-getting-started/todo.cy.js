6d1ecf4ee17aa5b89e3370bda6a1d2728d5ef9b4
flectric <muhamedkljajic@gmail.com>
2024-11-21 00:48:58 +0100

    added first test

--- cypress/e2e/1-getting-started/todo.cy.js
+++ cypress/e2e/1-getting-started/todo.cy.js
@@ -16,21 +16,13 @@ describe('example to-do app', () => {
     // so we must tell it to visit our website with the `cy.visit()` command.
     // Since we want to visit the same URL at the start of all our tests,
     // we include it in our beforeEach function so that it runs before each test
-    cy.visit('https://example.cypress.io/todo')
+    cy.visit('https://automationexercise.com/')
   })
 
-  it('displays two todo items by default', () => {
-    // We use the `cy.get()` command to get all elements that match the selector.
-    // Then, we use `should` to assert that there are two matched items,
-    // which are the two default items.
-    cy.get('.todo-list li').should('have.length', 2)
-
-    // We can go even further and check that the default todos each contain
-    // the correct text. We use the `first` and `last` functions
-    // to get just the first and last matched elements individually,
-    // and then perform an assertion with `should`.
-    cy.get('.todo-list li').first().should('have.text', 'Pay electric bill')
-    cy.get('.todo-list li').last().should('have.text', 'Walk the dog')
+  it.only('First test', () => {
+    cy.get('img[src*="logo.png"]').should('be.visible').click()
+    cy.get('a[href="/view_cart"]').first().click()
+    
   })
 
   it('can add new todo items', () => {

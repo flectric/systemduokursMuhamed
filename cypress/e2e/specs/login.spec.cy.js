550e5b2dceed71c2530d86b26cbf864bde02c12e
flectric <muhamedkljajic@gmail.com>
2024-12-16 23:32:49 +0100

    added new tests for registration and products

---
+++ cypress/e2e/specs/login.spec.cy.js
@@ -0,0 +1,17 @@
+describe('Login tests', () => {
+  beforeEach(() => {
+    // visit page and register
+  })
+  it('Login', () => {
+    // navigate to login page
+    // input email and password
+    // click login button
+    // success login
+  })
+  it('Login unsuccesfull', () => {
+    // navigate to login page
+    // input email and wrong password
+    // click login button
+    // find error message with text incorrect email or passwrod
+  })
+})
